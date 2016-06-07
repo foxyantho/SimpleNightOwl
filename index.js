e487b624009d6d0bf5f70cac5c7e4c317c7611d5
foxyantho <hello@iamafox.fr>
2016-06-07 02:37:55 +0200

    Implement pagemod+simili class

--- index.js
+++ index.js
@@ -1,51 +1,138 @@
 
-var self = require("sdk/self");
-
-var { ToggleButton } = require("sdk/ui/button/toggle");
+var { ToggleButton } = require('sdk/ui/button/toggle');
+var { PageMod } = require('sdk/page-mod');
 
 var prefs = require('sdk/simple-prefs');
-var setting = prefs.prefs;
 
-var pageMod = require("sdk/page-mod");
-
-
-
-function d(x){console.log(x);}
+var data = require('sdk/self').data;
 
 
 
-var button = ToggleButton({
-    id: "simplenighttime-button",
-    label: "Enable/disable night theme",
-    icon: "./icons/svg/sunny-day.svg",
-    onChange: buttonChange
+var page;
+
+
+
+function Button()
+{
+    this.button = ToggleButton(
+    {
+        id: 'simplenighttime-button',
+        label: 'Enable/disable night theme',
+        icon: './icons/svg/sunny-day.svg',
+        onChange: buttonChange
+    });
+}
+Button.prototype =
+{
+    constructor: Button,
+
+    check: function()
+    {
+        this.button.label = 'Disable night theme',
+
+        this.button.icon = './icons/svg/half-moon.svg';
+    },
+    uncheck: function()
+    {
+        this.button.label = 'Enable night theme',
+
+        this.button.icon = './icons/svg/sunny-day.svg';
+    }
+}
+
+var button = new Button();
+
+
+var config =
+{
+    get theme() { return prefs.prefs['theme']; },
+    set theme( value ) { prefs.prefs['theme'] = value; },
+    
+    get enabled() { return prefs.prefs['enabled']; },
+    set enabled( value ) { prefs.prefs['enabled'] = value; }
+}
+
+if( config.enabled )
+{
+    button.state('window', { checked: true });
+
+    applyTheme();
+}
+
+
+function applyOnEnabled()
+{
+    if( config.enabled )
+    {
+        button.check();
+
+        applyTheme();
+    }
+    else
+    {
+        button.uncheck();
+
+        removeTheme();
+    }
+}
+
+
+// prefs change event
+
+prefs.on('enabled', function( key ){
+    applyOnEnabled();
 });
 
 
 function buttonChange(state)
 {
-    if (state.checked) {
-        console.log(state);
+    if( state.checked )
+    {
+        config.enabled = true;
+    }
+    else
+    {
+        config.enabled = false;
     }
 }
 
 function applyTheme()
 {
-    pageMod.PageMod({
-        include: ["*"],
-        contentStyleFile: './css.css'
+    page = PageMod({
+        include: ['*','about:*', 'file://*'], // * -> http, https, ftp, 
+        contentStyleFile: './css/' + config.theme + '.css'
     });
+
+    console.log("page mod on");
 }
 
-
-
-function prefsEnabled( key )
+function removeTheme()
 {
-    button.state('window', { checked: setting[key] }); //bool
+    page.destroy();
+
+    console.log("page mod off");
 }
 
-prefs.on("enabled", prefsEnabled);
 
 
+/*
+Old low-level API :
 
+var { Cc, Ci } = require("chrome"),
+    nsIIOService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService),
+    nsIStyleSheetService = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService);
 
+var theme = nsIIOService.newURI(data.url('./css/' + __theme + '.css'), null, null);
+
+function applyTheme() {
+    if (!nsIStyleSheetService.sheetRegistered(getTheme(), nsIStyleSheetService.USER_SHEET)) {
+        nsIStyleSheetService.loadAndRegisterSheet(getTheme(), nsIStyleSheetService.USER_SHEET);
+    }
+}
+
+function removeTheme() {
+    if (nsIStyleSheetService.sheetRegistered(getTheme(), nsIStyleSheetService.USER_SHEET)) {
+        nsIStyleSheetService.unregisterSheet(getTheme(), nsIStyleSheetService.USER_SHEET);
+    }
+}
+*/
